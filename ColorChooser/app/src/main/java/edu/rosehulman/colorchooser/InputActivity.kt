62ed0f08a41bc356aece2592b6f60c15251afd42
Matt Boutell <boutell@gmail.com>
2020-01-05 21:29:52 -0500

    Update gradle, migrate to AndroidX

--- ColorChooser/app/src/main/java/edu/rosehulman/colorchooser/InputActivity.kt
+++ ColorChooser/app/src/main/java/edu/rosehulman/colorchooser/InputActivity.kt
@@ -2,7 +2,7 @@ package edu.rosehulman.colorchooser
 
 import android.graphics.Color
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import com.flask.colorpicker.ColorPickerView
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder
